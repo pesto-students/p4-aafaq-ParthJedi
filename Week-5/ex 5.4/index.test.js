631d9ad4b0a561a9c5071a4b28e2ae4cb73f9fce
Parth Joshi <parth.00.joshi@gmail.com>
2022-09-01 18:42:04 +0530

    negative numbers test cases added

--- Week-5/ex 5.4/index.test.js
+++ Week-5/ex 5.4/index.test.js
@@ -5,12 +5,24 @@ describe('Test suite for calculator functions', () => {
 		const value = mathOperations.sum(9, 16);
 		expect(value).toBe(25);
 	});
+	test('Test for summation with negative numbers', () => {
+		const value = mathOperations.sum(-9, -16);
+		expect(value).toBe(-25);
+	});
 	test('Test for difference', () => {
 		const value = mathOperations.diff(5, 5);
+		expect(value).toBe(0);
+	});
+	test('Test for difference with negative numbers', () => {
+		const value = mathOperations.diff(-5, -5);
 		expect(value).toBe(0);
 	});
 	test('Test for multiplication', () => {
 		const value = mathOperations.product(2, 2);
 		expect(value).toBe(4);
+	});
+	test('Test for multiplication with negative numbers', () => {
+		const value = mathOperations.product(-2, 2);
+		expect(value).toBe(-4);
 	});
 });
