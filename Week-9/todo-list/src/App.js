e5d7a92224b5ee537621ed858a28b63a5d89d69b
Parth Joshi <parth.00.joshi@gmail.com>
2022-10-02 03:43:07 +0530

    Week-9 assignment

--- Week-9/todo-list/src/App.js
+++ Week-9/todo-list/src/App.js
@@ -6,33 +6,39 @@ import TodoList from './components/TodoList';
 function App() {
 	const inputTodoRef = React.useRef(null);
 	const [todo, addTodo] = React.useState([]);
+	const todoValue = {};
 
 	function handleTodoAddition() {
-		const todoValue = inputTodoRef.current.value;
-		if (todoValue === '') {
+		todoValue.id = Date.now();
+		todoValue.name = inputTodoRef.current.value;
+		if (todoValue.name === '') {
 			window.alert('Please Enter a Valid Value');
 			return null;
 		} else {
 			addTodo((prevTodo) => {
 				return [...prevTodo, todoValue];
 			});
-			console.log(todo);
 			inputTodoRef.current.value = null;
 		}
 	}
 
 	return (
 		<div className='App'>
-			<header>TodoList Assignment</header>
-			<hr />
+			<h1>TodoList Assignment</h1>
 			<div className='todo-input'>
-				<label>Add your todo list: </label>
-				<input type='text' placeholder='enter a todo item' ref={inputTodoRef} />
+				<label>Add your todo list:</label>
+				&nbsp;&nbsp;&nbsp;
+				<input
+					type='text'
+					className='textAreaInput'
+					placeholder='enter a todo item'
+					ref={inputTodoRef}
+				/>
+				&nbsp;&nbsp;&nbsp;
 				<button onClick={handleTodoAddition}>Submit</button>
 			</div>
 			<hr />
-			{todo}
-			<TodoList />
+			<TodoList todo={todo} addTodo={addTodo} />
 		</div>
 	);
 }
